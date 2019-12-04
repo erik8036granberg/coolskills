5a8efdd52d30874bd5c0444b7598b9dc83e13248
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-12-04 17:48:11 +0100

    mobile menu ok

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,5 @@
+let mobileMenu = "closed";
+
 document.addEventListener("DOMContentLoaded", function(event) {
   navMenu();
   //   fadeMenu();
@@ -11,23 +13,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function navMenu() {
   document.querySelector(".menu").addEventListener("click", openMenu);
+  //   document.querySelector("nav ul").addEventListener("click", closeMenu);
 }
 
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+const nav_ul = document.querySelector("nav ul");
+
 function openMenu() {
-  document.querySelector(".menu").classList.add("change");
-  document.querySelector(".menu").removeEventListener("click", openMenu);
-  document.querySelector(".menu").addEventListener("click", closeMenu);
+  console.log("menu open");
+  menu.classList.add("change");
+  menu.removeEventListener("click", openMenu);
+  menu.addEventListener("click", closeMenu);
+  nav.classList.remove("up");
+  nav.classList.add("down");
   if (window.innerWidth < 900) {
+    setTimeout(() => {
+      nav_ul.classList.remove("nav_up");
+      nav_ul.classList.add("nav_down");
+    }, 250);
   }
   mobileMenu = "open";
 }
 
 function closeMenu() {
-  document.querySelector(".menu").removeEventListener("click", closeMenu);
-  document.querySelector(".menu").addEventListener("click", openMenu);
-  document.querySelector(".menu").classList.remove("change");
+  console.log("menu closed");
+  menu.removeEventListener("click", closeMenu);
+  menu.addEventListener("click", openMenu);
+  menu.classList.remove("change");
+  nav_ul.classList.remove("nav_down");
+  nav_ul.classList.add("nav_up");
   if (window.innerWidth < 900) {
+    setTimeout(() => {
+      nav.classList.remove("down");
+      nav.classList.add("up");
+    }, 250);
   }
-  document.querySelector("html").classList.remove("fixed");
   mobileMenu = "closed";
 }
