b2bb6f3861ab92d3dd14876b7bfb2355fffc18ac
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-12-06 09:20:53 +0100

    technology accordion ok

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   links();
+  accordion();
 }
 
 function links() {
@@ -13,4 +14,16 @@ function links() {
   document.querySelector(".more").addEventListener("click", () => {
     window.location = "index.html#koncept";
   });
+}
+
+function accordion() {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("show");
+    };
+  }
 }
