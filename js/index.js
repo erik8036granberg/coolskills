6d38f493abe324fa11e07d8828a5b2ba7ef9fef9
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-12-07 18:11:36 +0100

    js detect window size and swop accordion OK

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 "use strict";
 
 let click;
+let windowWidth;
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   links();
   accordion();
+  resize();
+  window.onresize = resize;
 }
 
 function links() {
@@ -16,10 +19,16 @@ function links() {
   });
 }
 
+function resize() {
+  windowWidth = window.innerWidth;
+  console.log(windowWidth);
+  accordion();
+}
+
 function accordion() {
   // based on w3schools Collapsibles/Accordion
 
-  if (window.innerWidth < 800) {
+  if (windowWidth < 800) {
     var acc = document.getElementsByClassName("accordion");
     var i;
 
@@ -31,6 +40,7 @@ function accordion() {
     }
 
     //discard mobile accordion styles
+    document.querySelector(".technologies").classList.remove("content");
     document.querySelector(".technologies").classList.add("mobile");
   } else {
     document.querySelector(".technologies").classList.remove("mobile");
