bfc1ef0896fbef17bd7a87833b45c9c8b68c9afc
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-12-13 13:57:11 +0100

    site kopi for wordpress linking made

---
+++ wordpress-tilkoblet/js/index2.js
@@ -0,0 +1,254 @@
+"use strict";
+
+let windowWidth;
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  links();
+  accordion();
+  resize();
+  window.onresize = resize;
+  runSplash();
+}
+
+function links() {
+  console.log("links");
+  document.querySelector(".more_wrapper").addEventListener("click", () => {
+    window.location = "index.html#koncept";
+  });
+  document.querySelector("#splash .heart").addEventListener("click", () => {
+    window.location = "index.html#bestil";
+  });
+}
+
+function resize() {
+  windowWidth = window.innerWidth;
+  console.log(windowWidth);
+  accordion();
+}
+
+function accordion() {
+  // based on w3schools Collapsibles/Accordion
+
+  if (windowWidth < 768) {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+      };
+    }
+
+    //discard mobile accordion styles
+    document.querySelector(".technologies").classList.remove("content");
+    document.querySelector(".technologies").classList.add("mobile");
+  } else {
+    document.querySelector(".technologies").classList.remove("mobile");
+    document.querySelector(".technologies").classList.add("content");
+  }
+}
+
+function runSplash() {
+  document.querySelector("#splash").classList.remove("hide");
+  console.log("runSplash");
+
+  document
+    .querySelector("#splash #fire_frame .bg_image")
+    .classList.remove("zoom");
+  document
+    .querySelector("#splash #postermagic .bg_image")
+    .classList.remove("zoom");
+  document
+    .querySelector("#splash #wondergirl .bg_image")
+    .classList.remove("zoom");
+  document
+    .querySelector("#splash #wondergirl .bg_image")
+    .classList.remove("zoom");
+
+  document.querySelector("#splash #fire_frame").classList.add("faded");
+  document.querySelector("#splash #skatertricks").classList.add("faded");
+  document.querySelector("#splash #postermagic").classList.add("faded");
+  document.querySelector("#splash #vinkler").classList.add("faded");
+  document.querySelector("#splash #wondergirl").classList.add("faded");
+  document.querySelector("#splash #fedt").classList.add("faded");
+  document.querySelector("#splash #support").classList.add("faded");
+  document.querySelector("#splash #streetsport").classList.add("faded");
+
+  document
+    .querySelector("#splash #fire_frame .splash_content")
+    .classList.add("faded");
+  document
+    .querySelector("#splash #skatertricks .splash_content")
+    .classList.add("faded");
+  document
+    .querySelector("#splash #vinkler .splash_content")
+    .classList.add("faded");
+  //run slides
+  slide1();
+}
+
+function slide1() {
+  swapBlack();
+  document.querySelector("#splash #fire_frame .bg_image").classList.add("zoom");
+  document.querySelector("#splash #fire_frame").classList.remove("faded");
+  setTimeout(function() {
+    document
+      .querySelector("#splash #fire_frame .splash_content")
+      .classList.remove("faded");
+    setTimeout(function() {
+      document
+        .querySelector("#splash #fire_frame .splash_content")
+        .classList.add("faded");
+      setTimeout(function() {
+        document.querySelector("#splash #fire_frame").classList.add("faded");
+      }, 1000);
+      setTimeout(function() {
+        slide2();
+      }, 500);
+    }, 3000);
+  }, 1000);
+}
+
+function slide2() {
+  swapWhite();
+  document.querySelector("#splash  #skatertricks").classList.remove("faded");
+  setTimeout(function() {
+    document
+      .querySelector("#splash  #skatertricks .splash_content")
+      .classList.remove("faded");
+    setTimeout(function() {
+      document
+        .querySelector("#splash  #skatertricks .splash_content")
+        .classList.add("faded");
+      setTimeout(function() {
+        document.querySelector("#splash  #skatertricks").classList.add("faded");
+      }, 1000);
+      setTimeout(function() {
+        slide3();
+      }, 500);
+    }, 2000);
+  }, 1000);
+}
+
+function slide3() {
+  swapBlack();
+  document
+    .querySelector("#splash #postermagic .bg_image")
+    .classList.add("zoom");
+  document.querySelector("#splash #postermagic").classList.remove("faded");
+  setTimeout(function() {
+    document.querySelector("#splash #postermagic").classList.add("faded");
+  }, 3000);
+  setTimeout(function() {
+    slide4();
+  }, 2500);
+}
+
+function slide4() {
+  swapWhite();
+  document.querySelector("#splash #vinkler").classList.remove("faded");
+  setTimeout(function() {
+    document
+      .querySelector("#splash #vinkler .splash_content")
+      .classList.remove("faded");
+    setTimeout(function() {
+      document
+        .querySelector("#splash #vinkler .splash_content")
+        .classList.add("faded");
+      setTimeout(function() {
+        document.querySelector("#splash #vinkler").classList.add("faded");
+      }, 1000);
+      setTimeout(function() {
+        slide5();
+      }, 500);
+    }, 2000);
+  }, 1000);
+}
+
+function slide5() {
+  swapBlack();
+  document.querySelector("#splash #wondergirl .bg_image").classList.add("zoom");
+  document.querySelector("#splash #wondergirl").classList.remove("faded");
+  setTimeout(function() {
+    document.querySelector("#splash #wondergirl").classList.add("faded");
+  }, 3000);
+  setTimeout(function() {
+    slide6();
+  }, 2500);
+}
+
+function slide6() {
+  swapWhite();
+  document.querySelector("#splash #fedt").classList.remove("faded");
+  setTimeout(function() {
+    document
+      .querySelector("#splash #fedt .splash_content")
+      .classList.remove("faded");
+    setTimeout(function() {
+      document
+        .querySelector("#splash #fedt .splash_content")
+        .classList.add("faded");
+      setTimeout(function() {
+        document.querySelector("#splash #fedt").classList.add("faded");
+      }, 1000);
+      setTimeout(function() {
+        slide7();
+      }, 500);
+    }, 2000);
+  }, 1000);
+}
+
+function slide7() {
+  swapBlack();
+  document.querySelector("#splash #support").classList.remove("faded");
+  setTimeout(function() {
+    document
+      .querySelector("#splash #support .splash_content")
+      .classList.remove("faded");
+    setTimeout(function() {
+      document
+        .querySelector("#splash #support .splash_content")
+        .classList.add("faded");
+      setTimeout(function() {
+        document.querySelector("#splash #support").classList.add("faded");
+      }, 1000);
+      setTimeout(function() {
+        slide8();
+      }, 500);
+    }, 3000);
+  }, 1000);
+}
+
+function slide8() {
+  swapWhite();
+  document.querySelector("#splash #streetsport").classList.remove("faded");
+  setTimeout(function() {
+    document
+      .querySelector("#splash #streetsport .splash_content")
+      .classList.remove("faded");
+    setTimeout(function() {
+      document
+        .querySelector("#splash #streetsport .splash_content")
+        .classList.add("faded");
+      setTimeout(function() {
+        document.querySelector("#splash #streetsport").classList.add("faded");
+        setTimeout(function() {
+          runSplash();
+        }, 2000);
+      }, 1000);
+    }, 2000);
+  }, 1000);
+}
+
+function swapWhite() {
+  document.querySelector(".more").classList.add("white_text");
+  document.querySelector(".more svg").style.fill = "#d5d5d4";
+}
+
+function swapBlack() {
+  document.querySelector(".more").classList.remove("white_text");
+  document.querySelector(".more svg").style.fill = "#101010";
+}
---
+++ wordpress-tilkoblet/js/nav2.js
@@ -0,0 +1,65 @@
+let mobileMenu = "closed";
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+const nav_ul = document.querySelector("nav ul");
+const logo = document.querySelector("#logo");
+
+document.addEventListener("DOMContentLoaded", event => {
+  navMenu();
+  scrolledMenu();
+  logo.addEventListener("click", () => {
+    window.location = "index.html#top";
+  });
+});
+
+function navMenu() {
+  document.querySelector(".menu").addEventListener("click", openMenu);
+  nav_ul.addEventListener("click", closeMenu);
+}
+
+function openMenu() {
+  console.log("menu open");
+  menu.classList.add("change");
+  menu.removeEventListener("click", openMenu);
+  menu.addEventListener("click", closeMenu);
+  nav.classList.add("down");
+  nav.classList.remove("in");
+  if (window.innerWidth < 900) {
+    nav_ul.classList.remove("nav_up");
+    nav_ul.classList.add("nav_down");
+    logo.classList.add("fadeout");
+  }
+  mobileMenu = "open";
+}
+
+function closeMenu() {
+  console.log("menu closed");
+  menu.removeEventListener("click", closeMenu);
+  menu.addEventListener("click", openMenu);
+  menu.classList.remove("change");
+  nav_ul.classList.remove("nav_down");
+  nav_ul.classList.add("nav_up");
+  if (window.innerWidth < 900) {
+    nav.classList.remove("down");
+    nav.classList.remove("in");
+    logo.classList.remove("fadeout");
+  }
+  mobileMenu = "closed";
+}
+
+function scrolledMenu() {
+  window.addEventListener("scroll", scrolled);
+
+  function scrolled() {
+    window.removeEventListener("scroll", scrolled);
+    if (mobileMenu !== "open") {
+      nav.classList.remove("in");
+      nav.classList.add("out");
+      setTimeout(function() {
+        nav.classList.remove("out");
+        nav.classList.add("in");
+        scrolledMenu();
+      }, 500);
+    }
+  }
+}
